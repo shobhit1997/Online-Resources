bddc04e2473e313c077ad2c594c89223df1f4300
shobhit1997 <shobhit.shobhitagarwal.agarwal@gmail.com>
2019-09-06 18:06:04 +0530

    backed data

--- server/app.js
+++ server/app.js
@@ -1,24 +1,30 @@
-const path= require('path');
-const publicPath = path.join(__dirname,'../public');
-const express=require('express');
-const  app=express();
-const bodyParser=require('body-parser');
-const loginRouter=require('./routes/loginRoutes');
-const resourceRouter=require('./routes/resourceRoutes');
-const uploadRouter=require('./routes/uploadRoutes');
-app.use(bodyParser.urlencoded({extended : true}));
+const path = require("path");
+const publicPath = path.join(__dirname, "../public");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const loginRouter = require("./routes/loginRoutes");
+const resourceRouter = require("./routes/resourceRoutes");
+const uploadRouter = require("./routes/uploadRoutes");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
-app.use(function(req,res,next){
-	res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Expose-Headers', 'x-auth');
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With,content-type, Accept , x-auth');
-  
+app.use(function(req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Expose-Headers", "x-auth");
+	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With,content-type, Accept , x-auth"
+	);
+
 	next();
 });
-
-app.use('/api/user',loginRouter);
-app.use('/api/resource',resourceRouter);
-app.use('/api/upload',uploadRouter);
-module.exports=app;
+app.get("/", function(req, res) {
+	// body...
+	res.send({ message: "Welcome" });
+});
+app.use("/api/user", loginRouter);
+app.use("/api/resource", resourceRouter);
+app.use("/api/upload", uploadRouter);
+module.exports = app;
